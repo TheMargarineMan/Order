2696a2e880af8d17dab988465dcae97f0b90fde2
Swapnil K Acharjee <ska1751@rit.edu>
2024-01-01 12:31:04 -0500

    resolved incorrect calls in entrypoint

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 su - postgres -c 'pg_ctl start -D /var/lib/postgres/data'
-cd /srv/Order/api && python server.py
-cd /srv/Order/order-ui && uvicorn main:app --reload
+cd /srv/Order/api && uvicorn main:app --reload
+cd /srv/Order/order-ui && npm run dev
