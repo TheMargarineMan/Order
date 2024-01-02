a1441de73f02cec9f543a219c6031aad5adf5605
Swapnil K Acharjee <ska1751@rit.edu>
2024-01-02 10:29:15 -0500

    modified to properly build instead of dev

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -2,4 +2,4 @@
 
 su - postgres -c 'pg_ctl start -D /var/lib/postgres/data' && \
 cd /srv/Order/api && uvicorn server:app --reload & \
-cd /srv/Order/order-ui && npm run dev
+cd /srv/Order/order-ui && npm run start
